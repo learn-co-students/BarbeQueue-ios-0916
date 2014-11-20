3927a19ef6d7a937a9ed2097e90b06e96f6fbe61
Chris Gonzales <gonzales.chris.a@gmail.com>
2014-11-20 15:35:31 -0500

    Initial Commit

---
+++ BarbeQueue/FISAppDelegate.h
@@ -0,0 +1,13 @@
+//
+//  FISAppDelegate.h
+//  BarbeQueue
+//
+//  Created by iOS Staff on 11/20/14
+//  Copyright (c) 2014 The Flatiron School. All rights reserved.
+//
+
+@interface FISAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
