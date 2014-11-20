8a428cdc500292edc30fbd249011771641b0a711
Chris Gonzales <gonzales.chris.a@gmail.com>
2014-11-20 15:38:18 -0500

    update pods with KIF

--- Podfile
+++ Podfile
@@ -1,12 +1,15 @@
-platform :ios, '7.0'
+# platform :ios, '7.0'
+#
+source 'https://github.com/CocoaPods/Specs.git'
 
 # Add Application pods here
 
 target :unit_tests, :exclusive => true do
   link_with 'UnitTests'
-  pod 'Specta'
+  pod 'Specta', :git => 'git@github.com:specta/specta.git'
   pod 'Expecta'
   pod 'OCMockito'
   pod 'Swizzlean'
   pod 'OHHTTPStubs'
+  pod 'KIF'
 end
--- Pods/Target Support Files/Pods-unit_tests/Pods-unit_tests-environment.h
+++ Pods/Target Support Files/Pods-unit_tests/Pods-unit_tests-environment.h
@@ -12,6 +12,18 @@
 #define COCOAPODS_VERSION_MINOR_Expecta 3
 #define COCOAPODS_VERSION_PATCH_Expecta 1
 
+// KIF
+#define COCOAPODS_POD_AVAILABLE_KIF
+#define COCOAPODS_VERSION_MAJOR_KIF 3
+#define COCOAPODS_VERSION_MINOR_KIF 0
+#define COCOAPODS_VERSION_PATCH_KIF 8
+
+// KIF/XCTest
+#define COCOAPODS_POD_AVAILABLE_KIF_XCTest
+#define COCOAPODS_VERSION_MAJOR_KIF_XCTest 3
+#define COCOAPODS_VERSION_MINOR_KIF_XCTest 0
+#define COCOAPODS_VERSION_PATCH_KIF_XCTest 8
+
 // OCHamcrest
 #define COCOAPODS_POD_AVAILABLE_OCHamcrest
 #define COCOAPODS_VERSION_MAJOR_OCHamcrest 4
@@ -38,9 +50,10 @@
 
 // Specta
 #define COCOAPODS_POD_AVAILABLE_Specta
-#define COCOAPODS_VERSION_MAJOR_Specta 0
-#define COCOAPODS_VERSION_MINOR_Specta 2
-#define COCOAPODS_VERSION_PATCH_Specta 1
+// This library does not follow semantic-versioning,
+// so we were not able to define version macros.
+// Please contact the author.
+// Version: 0.3.0.beta1.
 
 // Swizzlean
 #define COCOAPODS_POD_AVAILABLE_Swizzlean
