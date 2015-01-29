f965cd2067b1c6bbf8f7a00201f4e834120a6a98
Chris Gonzales <gonzales.chris.a@gmail.com>
2015-01-29 17:03:40 -0500

    adds empty FISIngredientStore

---
+++ BarbeQueue/FISIngredientStore.m
@@ -0,0 +1,13 @@
+//
+//  FISIngredientStore.m
+//  BarbeQueue
+//
+//  Created by Chris Gonzales on 1/29/15.
+//  Copyright (c) 2015 The Flatiron School. All rights reserved.
+//
+
+#import "FISIngredientStore.h"
+
+@implementation FISIngredientStore
+
+@end
